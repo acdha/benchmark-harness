6a6571eabb811a525c04fed54dd04bece426081f
Lennart Regebro <regebro@gmail.com>
2013-05-02 07:08:04 +0200

    Python 3 support

--- benchmark_harness/stats.py
+++ benchmark_harness/stats.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
-
+from __future__ import print_function
 
 def display_stats(name, times):
-    print "%s: completed %d trials" % (name, len(times))
-    print "\tMin: %0.3f" % min(times)
-    print "\tMax: %0.3f" % max(times)
+    print("%s: completed %d trials" % (name, len(times)))
+    print("\tMin: %0.3f" % min(times))
+    print("\tMax: %0.3f" % max(times))
